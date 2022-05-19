b244df05ccda45bed09ef75a4ec7c355bb59a41f
Sergi <sergigarciatocados1@gmail.com>
2022-05-19 20:55:54 +0200

    Login acabado comenzando funcionamiento juego

--- AndroidProject/app/src/main/java/com/tocadosmarin/fourinline/BoardSelection.java
+++ AndroidProject/app/src/main/java/com/tocadosmarin/fourinline/BoardSelection.java
@@ -1,14 +1,58 @@
 package com.tocadosmarin.fourinline;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class BoardSelection extends AppCompatActivity {
+    private Button btPlayGame;
+    private RadioGroup rgBoardSize;
+    private RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_selection);
+
+        btPlayGame = findViewById(R.id.btPlayGame);
+        rgBoardSize = findViewById(R.id.rgBoardSize);
+
+        btPlayGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = rgBoardSize.getCheckedRadioButtonId();
+                if (id == -1) {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(view.getContext());
+                    dialog.setTitle(R.string.dialog_title);
+                    dialog.setMessage(R.string.dialog_message);
+                    dialog.setCancelable(false);
+                    dialog.setPositiveButton(R.string.dialog_button, new
+                            DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialogo1, int id) {
+                                }
+                            });
+                    dialog.show();
+                } else {
+                    int selectedId = rgBoardSize.getCheckedRadioButtonId();
+                    radioButton = (RadioButton) findViewById(selectedId);
+
+                    SharedPreferences.Editor editor = MainActivity.pref.edit();
+                    editor.putString("board_size",radioButton.getText().toString());
+                    editor.commit();
+
+                    Intent i = new Intent(getApplicationContext(), Game.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
 }
--- AndroidProject/app/src/main/java/com/tocadosmarin/fourinline/Game.java
+++ AndroidProject/app/src/main/java/com/tocadosmarin/fourinline/Game.java
@@ -3,15 +3,67 @@ package com.tocadosmarin.fourinline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Game extends AppCompatActivity {
+    private LinearLayout mainLayout;
+    private HashMap<String, LinearLayout> layouts = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        TextView tv2 = findViewById(R.id.tv2);
-        tv2.setText(UserLogin.encryptedPref.getString("user",null));
+
+        mainLayout = findViewById(R.id.mainLayout);
+        //Toast.makeText(this, String.valueOf(mainLayout.getGravity()), Toast.LENGTH_SHORT).show();
+
+        //TODO buscar partida con otro jugador que haya seleccionado el mismo tamaño de tablero, una vez encontrada:
+        prepareBoard();
+        addLayoutListeners();
+    }
+
+    private void prepareBoard() {
+        String board_size = MainActivity.pref.getString("board_size", "?");
+        int size = Integer.valueOf(board_size.substring(0, 1));
+        for (int i = 0; i < size; i++) {
+            LinearLayout linearLayout = new LinearLayout(this);
+            linearLayout.setLayoutParams(setLayoutParams());
+            linearLayout.setGravity(81);
+            TextView tv = new TextView(this);
+            tv.setText("prueba");
+            linearLayout.addView(tv);
+            mainLayout.addView(linearLayout);
+            layouts.put("linearLayout" + i, linearLayout);
+        }
+    }
+
+    private LinearLayout.LayoutParams setLayoutParams() {
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+        );
+        return param;
+    }
+
+    private void addLayoutListeners() {
+        for (LinearLayout layout : layouts.values()) {
+            layout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TextView tv = new TextView(view.getContext());
+                    tv.setText("TextOnclick");
+                    layout.addView(tv);
+                }
+            });
+        }
     }
 }
