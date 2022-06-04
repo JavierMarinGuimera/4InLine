9927d2f6fa1850adee0469b14e3b1e7c7f5f38f0
Javier Marín <marinj460@gmail.com>
2022-06-04 19:54:13 +0200

    Archivo db.properties en gitignore + base de datos actualizada

--- 4inline.sql
+++ 4inline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-05-2022 a las 18:40:09
+-- Tiempo de generación: 04-06-2022 a las 19:51:46
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -30,18 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `scores` (
   `id` int(6) NOT NULL,
   `user_id` int(6) NOT NULL,
-  `score` int(5) NOT NULL
+  `score` int(5) NOT NULL,
+  `is_winner` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `scores`
 --
 
-INSERT INTO `scores` (`id`, `user_id`, `score`) VALUES
-(1, 3, -122),
-(2, 3, -121),
-(3, 3, -121),
-(4, 3, -120);
+INSERT INTO `scores` (`id`, `user_id`, `score`, `is_winner`) VALUES
+(25, 3, 450, 1),
+(26, 3, 450, 0),
+(27, 3, 450, NULL);
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,9 @@ CREATE TABLE `tokens` (
 --
 
 INSERT INTO `tokens` (`id`, `user_id`, `current_token`, `creation_date`) VALUES
-(1, 3, 'GFSKWesLCmZQ3XkUq0ZEYmM-p2ElKWggQslMXKwsIYHQPB0kidAcC4uFtxsA762U3APagk1Lm47gzdfK3hjsMA==', '2022-05-23 14:35:13');
+(1, 3, 'wRic6qvjQn2tLKCsC7sHoT_XTuVLg00mpVdT4mmMyRIT2WDcXQrnL5Yu77_4YC_F_P6hgXZyjAqo2NdAObMPXQ==', '2022-05-31 17:38:44'),
+(2, 4, '8WOZSZBR7o4071lyXfLKyrOtjOCiLD9ZM4X8NzrWzc5BOVXhD02ZOhqXNbKIuhksDhSXPiE-hLItrV0Md7nV3A==', '2022-05-28 20:51:13'),
+(3, 1, 'BjH0mXew6Kb33eKs6N12APX0n--rz6ERlEong6sDYyi29FQLWv_7stACyqKenXvB5zwCGYW-ynXvPzF2QNrWEw==', '2022-05-28 20:53:08');
 
 -- --------------------------------------------------------
 
@@ -80,7 +82,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(3, 'Javier', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
+(1, 'BxJpE7Ts2QFGAuhdVTyY2jvmZTzdIu', 'b599cd5a7103db5c8388cc81bafa20b783f9ad6c490b14d701217b685c754da4'),
+(3, 'Javier', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5'),
+(4, 'Sergi', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
 
 --
 -- Índices para tablas volcadas
@@ -114,19 +118,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `scores`
 --
 ALTER TABLE `scores`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
@@ -142,7 +146,7 @@ ALTER TABLE `scores`
 -- Filtros para la tabla `tokens`
 --
 ALTER TABLE `tokens`
-  ADD CONSTRAINT `tokens-users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `tokens-users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
